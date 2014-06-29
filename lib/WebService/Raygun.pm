e18085a9d1aba30b6fe67c3d92b2406a42901c84
William Travis Holton <heytrav@cpan.org>
2014-06-29 11:27:01 +0000

    Import of HEYTRAV/WebService-Raygun-0.008 from CPAN.
    gitpan-cpan-distribution: WebService-Raygun
    gitpan-cpan-version:      0.008
    gitpan-cpan-path:         HEYTRAV/WebService-Raygun-0.008.tar.gz
    gitpan-cpan-author:       HEYTRAV
    gitpan-cpan-maturity:     released

--- lib/WebService/Raygun.pm
+++ lib/WebService/Raygun.pm
@@ -6,170 +6,43 @@ WebService::Raygun - Connect to the Raygun.io API
 
 =head1 SYNOPSIS
 
-  use WebService::Raygun::Message;
   use WebService::Raygun::Messenger;
 
-   # see WebService::Raygun::Message for details of request object.
-    my $message = WebService::Raygun::Message->new(
-        user => 'null@null.com',
-        client => {
-            name      => 'something',
-            version   => 2,
-            clientUrl => 'www.null.com'
-        },
-        error       => {
-            stack_trace => [
-                {
-                line_number => 34
-                }
-            ]
-        },
-        environment => {
-            processor_count       => <integer>,
-            cpu                   => <integer>,
-            architecture          => <integer>,
-            total_physical_memory => <integer>
-        },
-        request => HTTP::Request->new(
-            POST => 'https://www.null.com',
-            [ 'Content-Type' => 'text/html', ]
-        ),
-    );
+    sub some_post_action {
+        my ( $self, $request ) = @_;
+        eval {
+            # do something with request
+            # ...
+        };
+        if ( my $exception = $@ ) {
 
-    my $message = $message->prepare_raygun;
-    my $raygun = WebService::Raygun::Messenger->new( api_key => '<your raygun.io api key>' );
-    my $response = $raygun->fire_raygun($message);
+            # see WebService::Raygun::Message for details
+            # of request object.
+            my $message = {
+                error   => $exception,
+                request => $request
+                user  => 'null@null.com',
+                client => {
+                    name      => 'something',
+                    version   => 2,
+                    clientUrl => 'www.null.com'
+                },
+            };
+
+            # initialise raygun.io messenger
+            my $raygun = WebService::Raygun::Messenger->new(
+                api_key => '<your raygun.io api key>',
+                message => $message
+            );
+            # send message to raygun.io
+            my $response = $raygun->fire_raygun;
+        }
+    }
+
 
 =head1 DESCRIPTION
 
 Interface for the Raygun.io API.
-
-
-=head1 DEPENDENCIES
-
-You will need to have an API key from raygun.io. By default, this module
-checks C<$ENV{RAYGUN_API_KEY}>.
-
-=over 2
-
-=item DateTime
-
-=back
-
-=over 2
-
-=item DateTime::Format::Strptime
-
-=back
-
-=over 2
-
-=item Filesys::DfPortable
-
-=back
-
-=over 2
-
-=item HTTP::Request
-
-=back
-
-=over 2
-
-=item JSON
-
-=back
-
-=over 2
-
-=item LWP::UserAgent
-
-=back
-
-
-=over 2
-
-=item Mouse
-
-=back
-
-=over 2
-
-=item Mouse::Util::TypeConstraints
-
-=back
-
-=over 2
-
-=item Mozilla::CA
-
-=back
-
-=over 2
-
-=item POSIX
-
-=back
-
-=over 2
-
-=item Sys::Info
-
-=back
-
-=over 2
-
-=item Sys::Info::OS
-
-=back
-
-=over 2
-
-=item URI
-
-=back
-
-=over 2
-
-=item Test::Class
-
-=back
-
-=over 2
-
-=item Test::Deep
-
-=back
-
-=over 2
-
-=item Test::Exception
-
-=back
-
-=over 2
-
-=item Test::More
-
-=back
-
-=over 2
-
-=item parent
-
-=back
-
-=over 2
-
-=item strict
-
-=back
-
-=over 2
-
-=item warnings
-
-=back
 
 
 =head1 SEE ALSO
