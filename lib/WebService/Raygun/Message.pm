a876877ae27cb8c195f8ff5655945a7fab94b0d1
William Travis Holton <heytrav@cpan.org>
2014-07-02 21:27:12 +0000

    Import of HEYTRAV/WebService-Raygun-0.016 from CPAN.
    gitpan-cpan-distribution: WebService-Raygun
    gitpan-cpan-version:      0.016
    gitpan-cpan-path:         HEYTRAV/WebService-Raygun-0.016.tar.gz
    gitpan-cpan-author:       HEYTRAV
    gitpan-cpan-maturity:     released

--- lib/WebService/Raygun/Message.pm
+++ lib/WebService/Raygun/Message.pm
@@ -91,50 +91,24 @@ use DateTime;
 use DateTime::Format::Strptime;
 use POSIX ();
 
-use Mouse::Util::TypeConstraints;
 use WebService::Raygun::Message::Error;
 use WebService::Raygun::Message::Request;
 use WebService::Raygun::Message::Environment;
 
-subtype 'MessageError' => as 'Object' => where {
-    $_->isa('WebService::Raygun::Message::Error');
+use Mouse::Util::TypeConstraints;
+
+subtype 'RaygunMessage' => as 'Object' => where {
+    $_->isa('WebService::Raygun::Message');
+};
+
+coerce 'RaygunMessage' => from 'HashRef' => via {
+    return WebService::Raygun::Message->new(%{$_});
 };
 
 subtype 'OccurredOnDateTime' => as 'Object' => where {
     $_->isa('DateTime');
 };
 
-subtype 'Request' => as 'Object' => where {
-    $_->isa('WebService::Raygun::Message::Request');
-};
-
-subtype 'Environment' => as 'Object' => where {
-    $_->isa('WebService::Raygun::Message::Environment');
-};
-
-subtype 'HttpRequest' => as 'Object' => where {
-    $_->isa('HTTP::Request');
-};
-
-subtype 'MojoliciousRequest' => as 'Object' => where {
-    $_->isa('Mojo::Message::Request');
-};
-
-subtype 'CatalystRequest' => as 'Object' => where {
-    $_->isa('Catalyst::Request');
-};
-
-subtype 'MooseException' => as 'Object' => where {
-    $_->isa('Moose::Exception');
-};
-
-subtype 'MojoException' => as 'Object' => where {
-    $_->isa('Mojo::Exception');
-};
-
-subtype 'DevelStacktrace' => as 'Object' => where {
-    $_->isa('Devel::StackTrace');
-};
 
 coerce 'OccurredOnDateTime' => from 'Str' => via {
     my $parser = DateTime::Format::Strptime->new(
@@ -147,135 +121,7 @@ coerce 'OccurredOnDateTime' => from 'Str' => via {
     return $parser->parse_datetime($_);
 };
 
-coerce 'Request' => from 'HttpRequest' => via {
-    my @header_names = $_->headers->header_field_names;
-    my $headers;
-    foreach my $header (@header_names) {
-        my $value = $_->header($header);
-        $headers->{$header} = $value;
-    }
-    my $query_string = $_->uri->query || '';
-
-    return WebService::Raygun::Message::Request->new(
-        url          => $_->uri->as_string,
-        raw_data     => $_->as_string,
-        headers      => $headers,
-        http_method  => $_->method,
-        query_string => $query_string,
-    );
-} => from 'MojoliciousRequest' => via {
-    my $headers      = $_->headers->to_hash;
-    my $query_params = $_->query_params;
-    my $query_string = '';
-    if (defined $query_params and $query_params->isa('Mojo::Parameters')) {
-        $query_string = $query_params->to_string;
-    }
-    return WebService::Raygun::Message::Request->new(
-        url          => $_->url->to_abs->path,
-        http_method  => $_->method,
-        raw_data     => $_->get_body_chunk,
-        headers      => $headers,
-        query_string => $query_string
-    );
-} => from 'CatalystRequest' => via {
-
-    my @header_names = $_->headers->header_field_names;
-    my $headers;
-    foreach my $header (@header_names) {
-        my $value = $_->header($header);
-        $headers->{$header} = $value;
-    }
-    my $chunk;
-    $_->read_chunk(\$chunk, 4096);
-    my $query_string = $_->uri->query || '';
-    return WebService::Raygun::Message::Request->new(
-        ip_address   => $_->address,
-        headers      => $headers,
-        http_method  => $_->method,
-        host_name    => $_->hostname,
-        raw_data     => $chunk,
-        query_string => $query_string,
-    );
-} => from 'HashRef' => via {
-    return WebService::Raygun::Message::Request->new(%{$_});
-};
-
-coerce 'Environment' => from 'HashRef' => via {
-    return WebService::Raygun::Message::Environment->new(%{$_});
-};
-
-coerce 'MessageError' => from 'HashRef' => via {
-    return WebService::Raygun::Message::Error->new(%{$_});
-} => from 'DevelStacktrace' => via {
-    my $stack_trace = __PACKAGE__->_iterate_stack_trace_frames($_);
-    return WebService::Raygun::Message::Error->new(
-        class_name  => (ref $_),
-        stack_trace => $stack_trace,
-    );
-} => from 'MooseException' => via {
-    my $trace       = $_->trace;
-    my $stack_trace = __PACKAGE__->_iterate_stack_trace_frames($trace);
-
-    return WebService::Raygun::Message::Error->new(
-        class_name  => (ref $_),
-        message     => $_->message,
-        stack_trace => $stack_trace,
-    );
-} => from 'MojoException' => via {
-
-    # Very basic for now since I can't find docs on what the frames look like
-    # in Mojo::Exception.
-    my $stack_trace = [ { line_number => $_->line, } ];
-    return WebService::Raygun::Message::Error->new(
-        class_name  => (ref $_),
-        message     => $_->message,
-        stack_trace => $stack_trace,
-    );
-} => from 'ArrayRef[Str]' => via {
-    my $error_text = join "\n" => @{$_};
-    my ($message, $stack_trace) = @{__PACKAGE__->_parse_exception_line($error_text)};
-
-    return WebService::Raygun::Message::Error->new(
-        stack_trace => $stack_trace,
-        message     => $message,
-    );
-
-} => from 'Str' => via {
-    my $error_text      = $_;
-    my ($message, $stack_trace) = @{__PACKAGE__->_parse_exception_line($error_text)};
-
-    return WebService::Raygun::Message::Error->new(
-        stack_trace => $stack_trace,
-        message     => $message,
-    );
-};
-
-=head2 _parse_exception_line
-
-Parse a text line into bits for a typical error.
-
-=cut
-
-sub _parse_exception_line {
-    my ($self, $error_text) = @_;
-    my $exception_regex = qr{
-        ^\s*(?<message> (?: (?! \sat\s [^\s]+ \s line).)*)
-        \sat\s(?<filename> (?: (?! \sline\s\d+).)* )
-        \sline\s(?<line>\d+)[^\d]*$
-    }xsm;
-    my ($message, $stack_trace);
-    while ($error_text =~ /$exception_regex/g) {
-        $message = $+{message} unless $message;
-        push @{$stack_trace}, {
-            line_number => $+{line},
-            file_name   => $+{filename} };
-    }
-    $message = $error_text unless $message;
-    if (not $stack_trace) {
-        $stack_trace = [ { line_number => 1 } ];
-    }
-    return [$message, $stack_trace];
-}
+no Mouse::Util::TypeConstraints;
 
 =head2 occurred_on
 
@@ -467,26 +313,6 @@ sub prepare_raygun {
     return $data;
 }
 
-=head2 _iterate_stack_trace_frames
-
-Iterate over frames in a L<Devel::StackTrace|Devel::StackTrace> like object.
-
-=cut
-
-sub _iterate_stack_trace_frames {
-    my ($self, $trace) = @_;
-    my $stack_trace = [];
-
-    while (my $frame = $trace->next_frame) {
-        push @{$stack_trace}, {
-            line_number => $frame->line,
-            class_name  => $frame->package,
-            file_name   => $frame->filename,
-            method_name => $frame->subroutine,
-            };
-    }
-    return $stack_trace;
-}
 
 =head1 DEPENDENCIES
 
